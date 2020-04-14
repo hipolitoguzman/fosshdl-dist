bb144c2bac0d3cc17bf7d3271cd1a9b62de6abcd
Hipolito Guzman <hguzman@us.es>
2020-04-15 00:12:27 +0200

    Updated some defaults in config.mk

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # Where to install 
 # If installing to system directories, change all "make install *" for "sudo
 # make install *"
-PREFIX = /opt/fosshdl-symbiotic
+PREFIX = /home/salas/opt/fosshdl
 
 # Set to "yes" if using the full yosys version with the Verific VHDL frontend,
 # set to any other value if using the open source yoys version (Verilog only).
@@ -9,19 +9,24 @@ USE_SYMBIOTIC = yes
 
 # If using full yosys, put the provided tar.gz in this directory and put here
 # the version number provided by SymbioticEDA
-SYMBIOTIC_VERSION = 20190312A
+SYMBIOTIC_VERSION = 20200402A-sevilla-university
 
 # Use a GCC version supported by GHDL (supported versions are listed on
 # https://ghdl.readthedocs.io/en/latest/building/gcc/index.html)
-GCC_VERSION = 7.3.0
+GCC_VERSION = 9.3.0
 
 # List of software to compile and install. Comment any one you don't want.
 selected += yosys
 selected += ghdl
 selected += uvvm 
-selected += arachne-pnr
+selected += osvvm 
+#selected += arachne-pnr
 selected += nextpnr
 selected += icestorm
+selected += cocotb
+
+# Latest UVVM version that compiles with GHDL
+UVVM_VERSION = v2019.09.02
 
 # Unsupported, but planned:
 #selected += migen
@@ -29,4 +34,4 @@ selected += icestorm
 #selected += verilator
 #selected += icestudio
 #selected += fusesoc
-#selected += fpga-knife
+#selected += theroshdl 
