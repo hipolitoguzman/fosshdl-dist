72bd0ba410ca16ddc39912b1ca6455c533d8bafd
Hipolito Guzman <hguzman@us.es>
2020-09-14 12:02:30 +0200

    Fix dependency install script

--- install_deps_ubuntu.sh
+++ install_deps_ubuntu.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-var="foo"
-echo $var
-
 COMMON_SW="git vim emacs lcov gtkwave octave"
 COMMON_DEPS="make build-essential g++"
 GHDL_DEPS="gnat texinfo zlib1g-dev"
@@ -9,6 +6,6 @@ COCOTB_DEPS="python3-pip"
 ICESTORM_DEPS="libftdi-dev"
 NEXTPNR_DEPS="cmake clang-format qt5-default python3-dev libboost-all-dev libeigen3-dev"
 VERILATOR_DEPS="autoconf flex bison libfl2 libfl-dev"
-IVERILOG_DEPTS="make g++ bison flex gperf libreadline-dev autoconf"
+IVERILOG_DEPS="make g++ bison flex gperf libreadline-dev autoconf"
 
 sudo apt install $COMMON_SW $COMMON_DEPS $GHDL_DEPS $COCOTB_DEPS $ICESTORM_DEPS $NEXTPNR_DEPS $VERILATOR_DEPS $IVERILOG_DEPS
