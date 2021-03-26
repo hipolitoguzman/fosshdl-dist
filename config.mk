cdecc06a252ac95d491d008c9e685ac4b363658b
salas <salas@trajano.us.es>
2021-03-26 22:38:17 +0100

    Update default config.mk

--- config.mk
+++ config.mk
@@ -8,7 +8,7 @@ SUDO = sudo
 
 # Set to "yes" if using the full yosys version with the Verific VHDL frontend,
 # set to any other value if using the open source yosys version (Verilog only).
-USE_SYMBIOTIC = yes
+USE_SYMBIOTIC = no
 
 # If using full yosys, put the provided tar.gz in this directory and put here
 # the version number provided by SymbioticEDA
@@ -16,11 +16,12 @@ SYMBIOTIC_VERSION = 20200902A-sevilla-university
 
 # Use a GCC version supported by GHDL (supported versions are listed on
 # https://ghdl.readthedocs.io/en/latest/building/gcc/index.html)
-GCC_VERSION = 9.3.0
+GCC_VERSION = 7.5.0
 
 # List of software to compile and install. Comment any one you don't want.
 selected += yosys
 selected += ghdl
+selected += ghdl-yosys-plugin
 #selected += uvvm
 #selected += osvvm
 #selected += arachne-pnr
