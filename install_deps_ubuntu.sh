18d898288aa665b2d96841f22fcb7057904256db
Hipolito Guzman <hguzman@us.es>
2021-11-09 17:32:39 +0100

    CI: add DEBIAN_FRONTEND=noninteractive

--- install_deps_ubuntu.sh
+++ install_deps_ubuntu.sh
@@ -9,4 +9,4 @@ NEXTPNR_DEPS="cmake clang-format qt5-default python3-dev libboost-all-dev libeig
 VERILATOR_DEPS="autoconf flex bison libfl2 libfl-dev"
 IVERILOG_DEPS="make g++ bison flex gperf libreadline-dev autoconf"
 
-apt install -y $COMMON_SW $COMMON_DEPS $YOSYS_DEPS $GHDL_DEPS $COCOTB_DEPS $ICESTORM_DEPS $NEXTPNR_DEPS $VERILATOR_DEPS $IVERILOG_DEPS
+DEBIAN_FRONTEND=noninteractive apt install -y $COMMON_SW $COMMON_DEPS $YOSYS_DEPS $GHDL_DEPS $COCOTB_DEPS $ICESTORM_DEPS $NEXTPNR_DEPS $VERILATOR_DEPS $IVERILOG_DEPS
