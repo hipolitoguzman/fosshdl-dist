32924b1c18e289dd979bec253be2ddbddf405c88
Hipolito Guzman <hguzman@us.es>
2022-01-22 04:33:59 +0100

    Bump GCC_VERSION to 8.4.0
    It seems that, when compiled with 7.5.0, new GHDL versions cannot
    generate code coverage

--- config-salas.mk
+++ config-salas.mk
@@ -9,7 +9,7 @@ PREFIX = /home/salas/fosshdl
 # Use a GCC version supported by GHDL (supported versions are listed on
 # https://ghdl.readthedocs.io/en/latest/building/gcc/index.html)
 # Keep in mind that not all gcc versions can generate code coverage with GHDL
-GCC_VERSION = 7.5.0
+GCC_VERSION = 8.4.0
 
 # List of software to compile and install. Comment any one you don't want.
 selected += yosys
