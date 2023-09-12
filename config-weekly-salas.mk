fdcb750f7080b13f260ce46f83066ba7b9820f89
Hipolito Guzman <hguzman@us.es>
2023-09-12 22:08:32 +0200

    Fixed PREFIX in config-weekly-salas.mk

--- config-weekly-salas.mk
+++ config-weekly-salas.mk
@@ -1,5 +1,5 @@
 # Where to install
-PREFIX = /home/hipolito/opt/fosshdl
+PREFIX = /home/salas/fosshdl
 
 # If your user has write permissions in the install location (such as when
 # installing in your $(HOME), you can comment the SUDO definition line below
