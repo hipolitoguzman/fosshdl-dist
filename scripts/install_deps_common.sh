e579e5dab6286585b470d7427a8ad31bd4e490c3
Hipolito Guzman <hguzman@us.es>
2025-04-04 01:50:02 +0200

    Pin CMake version, since boolector cannot be built with CMake 4.0

--- scripts/install_deps_common.sh
+++ scripts/install_deps_common.sh
@@ -1,7 +1,9 @@
 # Install common dependencies
 
-# Install cmake 3.25, needed by latest nextpnr and not provided by older
+# Install cmake 3.25+, needed by latest nextpnr and not provided by older
 # distributions (debian 11, ubuntu 18.04 and 22.04)
+# But not cmake 4 becuase it breaks the boolector build
+CMAKE_VERSION=3.31.6-0kitware1ubuntu24.04.1
 DEBIAN_FRONTEND=noninteractive apt update
 apt install -y software-properties-common
 add-apt-repository -y ppa:deadsnakes/ppa
@@ -9,5 +11,6 @@ apt install -y lsb-release wget gnupg
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor -o /usr/share/keyrings/kitware-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/kitware.list >/dev/null
 DEBIAN_FRONTEND=noninteractive apt update
-apt install -y cmake
+apt install -y cmake-data=$CMAKE_VERSION
+apt install -y cmake=$CMAKE_VERSION
 
