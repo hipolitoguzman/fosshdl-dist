64864d0be0ba87b6076c3416419f6f3eb6cd08e1
Hipolito Guzman <hguzman@us.es>
2025-04-04 10:34:58 +0200

    Use a wildcard when installing cmake
    This way the installation script is non-distro-specific

--- scripts/install_deps_common.sh
+++ scripts/install_deps_common.sh
@@ -3,7 +3,8 @@
 # Install cmake 3.25+, needed by latest nextpnr and not provided by older
 # distributions (debian 11, ubuntu 18.04 and 22.04)
 # But not cmake 4 becuase it breaks the boolector build
-CMAKE_VERSION=3.31.6-0kitware1ubuntu24.04.1
+CMAKE_VERSION=3.*
+#CMAKE_VERSION=3.31.6-0kitware1ubuntu24.04.1
 DEBIAN_FRONTEND=noninteractive apt update
 apt install -y software-properties-common
 add-apt-repository -y ppa:deadsnakes/ppa
