e3336ae2f42f30f2bcf373a67e897e47c3b4e4f4
Hipolito Guzman <hguzman@us.es>
2025-07-11 01:35:02 +0200

    Update verilator dependencies

--- scripts/install_deps_ubuntu:18.04.sh
+++ scripts/install_deps_ubuntu:18.04.sh
@@ -7,7 +7,7 @@ GHDL_DEPS="wget gnat texinfo zlib1g-dev"
 COCOTB_DEPS="python3-pip"
 ICESTORM_DEPS="libftdi-dev"
 NEXTPNR_DEPS="clang-format qt5-default python3-dev libboost-all-dev libeigen3-dev"
-VERILATOR_DEPS="autoconf flex bison libfl2 libfl-dev"
+VERILATOR_DEPS="autoconf flex bison libfl2 libfl-dev help2man"
 IVERILOG_DEPS="make g++ bison flex gperf libreadline-dev autoconf"
 
 DEBIAN_FRONTEND=noninteractive apt install -y $COMMON_SW $COMMON_DEPS $YOSYS_DEPS $SBY_DEPS $GHDL_DEPS $COCOTB_DEPS $ICESTORM_DEPS $NEXTPNR_DEPS $VERILATOR_DEPS $IVERILOG_DEPS
