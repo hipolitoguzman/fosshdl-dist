1117d67b310b639f336bac194de257e670934558
Hipolito Guzman <hguzman@us.es>
2026-02-04 20:17:36 +0100

    Add nvc dependencies to scripts/install_deps*

--- scripts/install_deps_ubuntu:24.04.sh
+++ scripts/install_deps_ubuntu:24.04.sh
@@ -4,10 +4,11 @@ COMMON_DEPS="make build-essential g++"
 YOSYS_DEPS="tcl tcl-dev clang"
 SBY_DEPS="libgmp-dev ninja-build python3-dev curl"
 GHDL_DEPS="wget gnat texinfo zlib1g-dev"
+NVC_DEPS="build-essential automake autoconf flex check llvm-dev pkg-config zlib1g-dev libdw-dev libffi-dev libzstd-dev"
 COCOTB_DEPS="python3-pip"
 ICESTORM_DEPS="libftdi-dev"
 NEXTPNR_DEPS="clang-format qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools python3-dev libboost-all-dev libeigen3-dev"
 VERILATOR_DEPS="autoconf flex bison libfl2 libfl-dev help2man"
 IVERILOG_DEPS="make g++ bison flex gperf libreadline-dev autoconf"
 
-DEBIAN_FRONTEND=noninteractive apt install -y $COMMON_SW $COMMON_DEPS $YOSYS_DEPS $SBY_DEPS $GHDL_DEPS $COCOTB_DEPS $ICESTORM_DEPS $NEXTPNR_DEPS $VERILATOR_DEPS $IVERILOG_DEPS
+DEBIAN_FRONTEND=noninteractive apt install -y $COMMON_SW $COMMON_DEPS $YOSYS_DEPS $SBY_DEPS $GHDL_DEPS $NVC_DEPS $COCOTB_DEPS $ICESTORM_DEPS $NEXTPNR_DEPS $VERILATOR_DEPS $IVERILOG_DEPS
