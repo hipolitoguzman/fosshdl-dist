a03b9ba19684875b6dd28462682ee920e745e9d6
Hipolito Guzman <hguzman@us.es>
2026-05-08 20:37:37 +0200

    Do not manage cmake installation in newer distros

--- config-opt.mk
+++ config-opt.mk
@@ -1,5 +1,5 @@
 # Where to install
-PREFIX = /usr/local/fosshdl
+PREFIX = /opt/fosshdl
 
 # If your user has write permissions in the install location (such as when
 # installing in your $(HOME), you can comment the SUDO definition line below
